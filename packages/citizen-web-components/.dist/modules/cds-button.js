187b7609903daf2adfd4d46920822b678f4a4215
Stephen James <sjames@salesforce.com>
2020-05-15 13:23:49 -0600

    Remove web components dist

--- packages/citizen-web-components/.dist/modules/cds-button.js
+++
@@ -1,3 +0,0 @@
-import './vendor-de46247d.chunk.js';
-export { B as Button } from './cds-9f29552a.chunk.js';
-//# sourceMappingURL=cds-button.js.map
--- packages/citizen-web-components/.dist/modules/cds-theme-provider.js
+++
@@ -1,3 +0,0 @@
-import './vendor-de46247d.chunk.js';
-export { T as ThemeProvider } from './cds-9f29552a.chunk.js';
-//# sourceMappingURL=cds-theme-provider.js.map
